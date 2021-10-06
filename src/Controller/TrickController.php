2517800fc2b88f9d4c063a3cdadd71a6e2f5016f
L-TCD <loic.coissard@gmail.com>
2021-10-06 16:54:07 +0200

    add slugTrickShow method in TrickController

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -15,65 +15,23 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/trick')]
 class TrickController extends AbstractController
 {
-    #[Route('/', name: 'trick_index', methods: ['GET'])]
-    public function index(TrickRepository $trickRepository): Response
-    {
-        return $this->render('trick/index.html.twig', [
-            'tricks' => $trickRepository->findAll(),
-        ]);
-    }
+	#[Route('/', name: 'trick_index', methods: ['GET'])]
+	public function index(TrickRepository $trickRepository): Response
+	{
+		return $this->render('trick/index.html.twig', [
+			'tricks' => $trickRepository->findAll(),
+		]);
+	}
 
-    #[Route('/new', name: 'trick_new', methods: ['GET', 'POST'])]
-    public function new(Request $request): Response
-    {
-        $trick = new Trick();
-        $form = $this->createForm(TrickType::class, $trick);
-        $form->handleRequest($request);
+	#[Route('/new', name: 'trick_new', methods: ['GET', 'POST'])]
+	public function new(Request $request): Response
+	{
+		$trick = new Trick();
+		$form = $this->createForm(TrickType::class, $trick);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+		if ($form->isSubmitted() && $form->isValid()) {
 
-			$images = $form->get('images')->getData();
-
-			foreach($images as $image){
-				$file = md5(uniqid()).'.'.$image->guessExtension();
-				$image->move(
-					$this->getParameter('images_directory'),
-					$file
-				);
-
-				$img = new Image();
-				$img->setName($file);
-				$trick->addImage($img);
-			}
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($trick);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('trick/new.html.twig', [
-            'trick' => $trick,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'trick_show', methods: ['GET'])]
-    public function show(Trick $trick): Response
-    {
-        return $this->render('trick/show.html.twig', [
-            'trick' => $trick,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'trick_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Trick $trick): Response
-    {
-        $form = $this->createForm(TrickType::class, $trick);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
 			$images = $form->get('images')->getData();
 
 			foreach ($images as $image) {
@@ -88,28 +46,70 @@ class TrickController extends AbstractController
 				$trick->addImage($img);
 			}
 
-            $this->getDoctrine()->getManager()->flush();
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->persist($trick);
+			$entityManager->flush();
 
-            return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
-        }
+			return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
+		}
 
-        return $this->renderForm('trick/edit.html.twig', [
-            'trick' => $trick,
-            'form' => $form,
-        ]);
-    }
+		return $this->renderForm('trick/new.html.twig', [
+			'trick' => $trick,
+			'form' => $form,
+		]);
+	}
 
-    #[Route('/{id}', name: 'trick_delete', methods: ['POST'])]
-    public function delete(Request $request, Trick $trick): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$trick->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($trick);
-            $entityManager->flush();
-        }
+	#[Route('/{id}', name: 'trick_show', methods: ['GET'])]
+	public function show(Trick $trick): Response
+	{
+		return $this->render('trick/show.html.twig', [
+			'trick' => $trick,
+		]);
+	}
 
-        return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
-    }
+	#[Route('/{id}/edit', name: 'trick_edit', methods: ['GET', 'POST'])]
+	public function edit(Request $request, Trick $trick): Response
+	{
+		$form = $this->createForm(TrickType::class, $trick);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$images = $form->get('images')->getData();
+
+			foreach ($images as $image) {
+				$file = md5(uniqid()) . '.' . $image->guessExtension();
+				$image->move(
+					$this->getParameter('images_directory'),
+					$file
+				);
+
+				$img = new Image();
+				$img->setName($file);
+				$trick->addImage($img);
+			}
+
+			$this->getDoctrine()->getManager()->flush();
+
+			return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
+		}
+
+		return $this->renderForm('trick/edit.html.twig', [
+			'trick' => $trick,
+			'form' => $form,
+		]);
+	}
+
+	#[Route('/{id}', name: 'trick_delete', methods: ['POST'])]
+	public function delete(Request $request, Trick $trick): Response
+	{
+		if ($this->isCsrfTokenValid('delete' . $trick->getId(), $request->request->get('_token'))) {
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->remove($trick);
+			$entityManager->flush();
+		}
+
+		return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
+	}
 
 	/**
 	 * @Route("/delete/image/{id}", name="trick_delete_image", methods={"DELETE"})
@@ -119,11 +119,11 @@ class TrickController extends AbstractController
 		$data = json_decode($request->getContent(), true);
 
 		// On vérifie si le token est valide
-		if($this->isCsrfTokenValid('delete'.$image->getId(), $data['_token'])){
+		if ($this->isCsrfTokenValid('delete' . $image->getId(), $data['_token'])) {
 			// On récupère le nom de l'image
 			$fileName = $image->getName();
 			// On supprime le fichier
-			unlink($this->getParameter('images_directory').'/'.$fileName);
+			unlink($this->getParameter('images_directory') . '/' . $fileName);
 
 			// On supprime l'entrée de la base
 			$em = $this->getDoctrine()->getManager();
@@ -135,5 +135,24 @@ class TrickController extends AbstractController
 		} else {
 			return new JsonResponse(['error' => 'Token Invalide'], 400);
 		}
+	}
+
+	/**
+	 * @Route("/fiche-detaillee/{slug}", name="slug_trick")
+	 */
+	public function slugTrickShow($slug, TrickRepository $trickRepository): Response
+	{
+
+		$trick = $trickRepository->findOneBy([
+			'slug' => $slug,
+		]);
+
+		if (!$trick) {
+			throw $this->createNotFoundException("La figure demandée n'existe pas.");
+		}
+
+		return $this->render('trick/show.html.twig', [
+			'trick' => $trick,
+		]);
 	}
 }
